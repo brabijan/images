6b86d6a477741903033a77fd3522c8ab6b851f18
chemiX <iam@chemix.cz>
2015-10-29 10:38:49 +0100

    support for Texy 2.8+ (with namespaces)
    new version of Texy use namespaces

--- src/Brabijan/Images/Macros/Texy.php
+++ src/Brabijan/Images/Macros/Texy.php
@@ -8,16 +8,30 @@ use Nette;
 
 class Texy extends Nette\Object
 {
-
-	public static function register(\Texy $texy, ImagePipe $imagePipe)
+	/**
+	 * @param \Texy|\Texy\Texy $texy
+	 * @param ImagePipe $imagePipe
+	 */
+	public static function register($texy, ImagePipe $imagePipe)
 	{
-		$texy->addHandler("image", function (\TexyHandlerInvocation $invocation, \TexyImage $image, $link) use ($imagePipe) {
+		if (!($texy instanceof \Texy) && !($texy instanceof \Texy\Texy)) {
+			throw new \InvalidArgumentException('The $texy parameter is not instance of Texy or Texy\Texy');
+		}
+
+		$texy->addHandler("image", function ($invocation, $image, $link) use ($imagePipe) {
+			if (!($invocation instanceof \TexyHandlerInvocation) && !($invocation instanceof \Texy\HandlerInvocation)) {
+				throw new \InvalidArgumentException('The $invocation parameter is not instance of TexyHandlerInvocation or Texy\HandlerInvocation');
+			}
+			if (!($image instanceof \TexyImage) && !($image instanceof \Texy\Image)) {
+				throw new \InvalidArgumentException('The $image parameter is not instance of TexyImage or Texy\Image');
+			}
+
 			$arguments = Helpers::prepareMacroArguments($image->URL);
 			try {
 				$image->URL = $imagePipe->setNamespace($arguments["namespace"])->request($arguments["name"], $arguments["size"], $arguments["flags"], TRUE);
 			} catch (FileNotFoundException $e) {
 				$image->URL = $arguments["name"];
-				if(!empty($arguments["size"])) {
+				if (!empty($arguments["size"])) {
 					list($image->width, $image->height) = explode("x", $arguments["size"]);
 				}
 			}
